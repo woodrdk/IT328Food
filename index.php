436f3d8d9ee4b7f0aac9983cad28b346ff26ce28
Rob Wood <rwood3@mail.greenriver.edu>
2020-01-22 13:55:17 -0500

    index update

--- index.php
+++ index.php
@@ -24,5 +24,40 @@ $f3 -> route('GET /breakfast', function(){
     echo $view->render('views/breakfast.html');
 });
 
+$f3 -> route('GET /lunch', function(){
+    $view = new Template();
+    echo $view->render('views/lunch.html');
+});
+
+$f3 -> route('GET /dinner', function(){
+    $view = new Template();
+    echo $view->render('views/dinner.html');
+});
+
+$f3 -> route('GET /@item', function($f3, $params){
+    // var_dump($params);
+    $item = $params['item'];
+    echo "<p>You ordered $item </p>";
+
+    $foodWeServe = array("tacos", "pizza", "lumpia");
+    if(!in_array($item, $foodWeServe)){
+        echo "<p>Sorry.. we dont serve $item </p>";
+    }
+
+    switch($item){
+        case 'tacos':
+            echo "<p>We serve tacos on Tuesdays</p>";
+            break;
+        case 'pizza':
+            echo "<p>We serve pizza on Fridays</p>";
+            break;
+        case 'lumpia';
+            $f3 -> reroute("/breakfast");
+        default:
+            $f3 -> error(404);
+    }
+});
+
+
 // Run Fat Free
 $f3 -> run();
