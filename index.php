97edea8e15ba0a26a288a0a7dbd98a2ddb2015dc
Rob Wood <rwood3@mail.greenriver.edu>
2020-01-22 13:01:56 -0500

    set up

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+// THIS IS OUR CONTROLLER
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// require the autoload file
+require_once ('vendor/autoload.php');
+
+// Create an instance of the base class
+$f3 = Base::instance();
+
+// define a default route
+$f3 -> route('GET /', function(){
+    $view = new Template();
+    echo $view->render('views/food.html');
+
+});
+// Run Fat Free
+$f3 -> run();
