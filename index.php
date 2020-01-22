a56401210860ff43740eb81da0e2ab1c1100c2fe
Rob Wood <rwood3@mail.greenriver.edu>
2020-01-22 13:35:23 -0500

    added routes

--- index.php
+++ index.php
@@ -15,8 +15,14 @@ $f3 = Base::instance();
 // define a default route
 $f3 -> route('GET /', function(){
     $view = new Template();
-    echo $view->render('views/food.html');
+    echo $view->render('views/home.html');
 
 });
+
+$f3 -> route('GET /breakfast', function(){
+    $view = new Template();
+    echo $view->render('views/breakfast.html');
+});
+
 // Run Fat Free
 $f3 -> run();
